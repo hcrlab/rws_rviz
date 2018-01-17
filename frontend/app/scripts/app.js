9f734f0fd82734e4372d2f65a292a3c8dcdce2e1
Justin Huang <jstn@cs.washington.edu>
2018-01-16 20:32:16 -0800

    Removed old code.

--- frontend/app/scripts/app.js
+++
@@ -1,7 +0,0 @@
-/*(function(document) {
-  'use strict';
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-  });
-})(document);*/
